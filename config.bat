936c1d3c7202f3de7022e0cd734a247e6d60c7ba
Sergio Chavez <ser_chg@hotmail.com>
2025-09-28 21:20:12 -0600

    se cambia disco de instalacion del servidor

--- config.bat
+++ config.bat
@@ -1,7 +1,7 @@
 @echo off
 :: Declaración de variables Globales
-set EXE_PATH="E:\SteamLibrary\steamapps\common\ARK Survival Ascended Dedicated Server\ShooterGame\Binaries\Win64\ArkAscendedServer.exe"
-set INI_PATH="E:\SteamLibrary\steamapps\common\ARK Survival Ascended Dedicated Server\ShooterGame\Saved\Config\WindowsServer\"
+set EXE_PATH="D:\SteamLibrary\steamapps\common\ARK Survival Ascended Dedicated Server\ShooterGame\Binaries\Win64\ArkAscendedServer.exe"
+set INI_PATH="D:\SteamLibrary\steamapps\common\ARK Survival Ascended Dedicated Server\ShooterGame\Saved\Config\WindowsServer\"
 set CLUSTER_ID=ShekoGaming
 set CLUSTER_DIR_OVERRIDE="E:\asaServer"
 set MODS=929800,940975,929420,928597,930494,935408,929912,942024,928661,950914,941697,928650,946536,928708
